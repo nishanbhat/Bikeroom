3a1b195dd6afe7343a64f2d58981468b0adec205
nishanbhat <rananishanbhat@gmail.com>
2021-03-24 20:53:51 +0545

    navbar and carousel added

--- Bikeroom/wsgi.py
+++ Bikeroom/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Bikeroom project.
+WSGI config for bikeroom project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bikeroom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bikeroom.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bikeroom.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bikeroom.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
